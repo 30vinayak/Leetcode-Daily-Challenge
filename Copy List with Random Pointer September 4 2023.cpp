a471c1f207076d14819bd4c1444d8062a0179322
Vinayak Agrawal <92200281+30vinayak@users.noreply.github.com>
2023-09-05 15:16:40 +0530

    Update Copy List with Random Pointer September 4 2023.cpp

--- Copy List with Random Pointer September 4 2023.cpp
+++ Copy List with Random Pointer September 4 2023.cpp
@@ -1,6 +1,5 @@
-Problem link: https://leetcode.com/problems/copy-list-with-random-pointer/description/
+##Problem link: [Copy List with Random Pointer](https://leetcode.com/problems/copy-list-with-random-pointer/description/)
 
-/*
 // Definition for a Node.
 class Node {
 public:
@@ -14,7 +13,7 @@ public:
         random = NULL;
     }
 };
-*/
+
 void nextPointer(Node* itr, Node* newItr, int index, map<Node*, int> &mp, map<int, Node*> &newmp){
     while(itr != NULL){
         mp[itr] = index;
